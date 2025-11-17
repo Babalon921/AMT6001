959bd3d1078e7e2c71ae14d9757beb05869e64a8
Babalon921 <anonusbuck1@gmail.com>
2025-11-17 11:11:51 +0000

    Add project files.

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,50 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+AMT6001AudioProcessorEditor::AMT6001AudioProcessorEditor (AMT6001AudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize(200, 200);
+    // these define the parameters of our slider object
+    midiVolume.setSliderStyle(juce::Slider::LinearBarVertical);
+    midiVolume.setRange(0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setColour(juce::Slider::trackColourId, juce::Colours::red);
+    midiVolume.setPopupDisplayEnabled(true, false, this);
+    midiVolume.setTextValueSuffix(" Volume");
+    midiVolume.setValue(1.0);
+    // this function adds the slider to the editor
+    addAndMakeVisible(&midiVolume);
+}
+
+AMT6001AudioProcessorEditor::~AMT6001AudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void AMT6001AudioProcessorEditor::paint (juce::Graphics& g)
+{
+    g.fillAll(juce::Colours::black);
+    // set the current drawing colour to black
+    g.setColour(juce::Colours::white);
+    // set the font size and draw text to the screen
+    g.setFont(15.0f);
+    g.drawFittedText("Midi Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
+
+}
+
+void AMT6001AudioProcessorEditor::resized()
+{
+    midiVolume.setBounds(40, 30, 20, getHeight() - 60);
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class AMT6001AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    AMT6001AudioProcessorEditor (AMT6001AudioProcessor&);
+    ~AMT6001AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    AMT6001AudioProcessor& audioProcessor;
+    juce::Slider midiVolume;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AMT6001AudioProcessorEditor)
+};
