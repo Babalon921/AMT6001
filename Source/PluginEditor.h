e5c163b9823b9a382871bf2314c2f2c5f5b1045a
Babalon921 <anonusbuck1@gmail.com>
2025-12-02 19:16:22 +0000

    add dials ready for implmentation

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -26,12 +26,35 @@ public:
 
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    AMT6001AudioProcessor& audioProcessor;
-    juce::Slider midiVolume;
+
+    // Changed Value Area
     void sliderValueChanged(juce::Slider*) override;
 
+    AMT6001AudioProcessor& audioProcessor;
+
+    //Slider Defs
+    juce::Slider midiVolume;
+
+    //adsr
+    juce::Slider attack_Dial;
+    juce::Slider decay_Dial;
+    juce::Slider sustain_Dial;
+    juce::Slider release_Dial;
+
+    //harmonics 
+    juce::Slider fund;
+    juce::Slider harmonic2;
+
+    //labels
+    juce::Label attack_label;
+    juce::Label decay_label;
+    juce::Label sustain_label;
+    juce::Label release_label;
+    juce::Label fund_label;
+    juce::Label harmonic2_label;
+
+
+    //midi keyboard
     juce::MidiKeyboardComponent virtualKeyboard{ audioProcessor.keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard };
 
 
